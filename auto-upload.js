6b75e3bbad0eb2806665b8960c3b2159d9b5f8f5
Dduvacher <dduvacher@gmail.com>
2017-08-30 14:32:09 +0200

    :gem: manage error and fix callback

--- auto-upload.js
+++ auto-upload.js
@@ -16,17 +16,21 @@ var q = async.queue(function(media, callback) {
 
     request.post({url:'http://localhost:6060/api/v1/actions/socialite', formData: formData}, function optionalCallback(err, httpResponse, body) {
     if (err) {
-        return console.error('upload failed:', err);
+        typeof callback === 'function' && callback(new Error('Upload failed: '+err));
+        }else{
+        if(body.match(/{"response":"OK".*/))
+        {
+            typeof callback === 'function' && callback(null, body);
+        }else{
+            typeof callback === 'function' && callback(new Error('Altruist failure.'+body))
+        }
     }
-    console.log('Upload successful!  Server responded with:', body);
-    success++;
-    });
 
-    callback();
+    });
 }, 10);
 
 q.drain = function() {
-    console.log('All the media have been uploaded');
+    console.log('All the media have been queued');
 };
 
 fs.readdir( path, function( err, files ) {
@@ -34,9 +38,16 @@ fs.readdir( path, function( err, files ) {
         console.error( "Could not list the directory.", err );
         process.exit( 1 );
     }
-    
     files.forEach(function(element)
     {
-        q.push(path+element);
+        q.push(path+element, function(err,result)
+        {
+            if(err)
+            {
+                console.log(err);
+            }else{
+                console.log('Upload successful!  Server responded with:', result);
+            }
+        });
     });
 });
